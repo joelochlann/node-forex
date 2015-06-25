90535285c3b728fb436170e80d26b5ceaa86a7fa
Joseph Smith <joseph.smith@worldfirst.com>
2015-06-25 11:59:47 +0000

    Re-arranged files after fixing conflicts

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res) {
+//    res.sendFile(__dirname + '/index.html');
+// });
 
 app.use(express.static('public'));
 
