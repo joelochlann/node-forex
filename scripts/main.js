b1319518309d697422b0c435bee8d8cb22a1d415
Dominic Orme <dominic.orme@worldfirst.com>
2015-06-25 10:38:31 +0000

    Constructs a websocket, outputs an error in the event it can't connect.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,22 @@
 (function(){
+	window.Platform = {
+		run:function(socketAddress) {
+			var statusBar = $(document).find("#status-bar");
 
+			var conn = new WebSocket(socketAddress);
+			conn.onopen = function(e) {
+				$(statusBar).html("Connection established");
+			};
+
+			conn.onmessage = function(e) {
+				$(statusBar).html(JSON.stringify(e.data));
+			    console.log(e.data);
+			};
+
+			conn.onerror = function(e) {
+				$(statusBar).html("Error - see console.");
+			    console.log(e);	
+			}
+		}
+	}
 })();
