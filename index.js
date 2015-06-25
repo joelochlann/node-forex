dbd88c27165d9608439e178102aee045204717b3
Joseph Smith <joseph.smith@worldfirst.com>
2015-06-25 10:30:54 +0000

    Serve up frontend with express

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+app.use(express.static('public'));
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
