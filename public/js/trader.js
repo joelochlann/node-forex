c67defb96a122fa5cc983b5a4375124cd240c01d
Dominic Orme <dominic.orme@worldfirst.com>
2015-06-25 14:39:18 +0000

    Fixed rounding errors in trader

--- public/js/trader.js
+++ public/js/trader.js
@@ -7,9 +7,12 @@
 		this.amount = 0;
 
 		this.trade = function(tick) {
-			if (this.buyFunction(tick)) {
+			if (this.buyFunction(tick) && this.balance > tick.close) {
 				this.amount = Math.floor(this.balance / tick.close);
-				this.balance -= this.amount * tick.close;
+				this.balance -= parseFloat("" + (this.amount * tick.close)).toFixed(2);
+			}
+			if (isNaN(this.amount) || isNaN(this.balance)) {
+				console.log("Amount and / or balance went NaN");
 			}
 			if (this.sellFunction(tick)) {
 				this.balance += this.amount * tick.close;
@@ -20,7 +23,7 @@
 		this.display = function(template) {
 			var context = {
 				title:this.title, 
-				balance:this.balance, 
+				balance:parseFloat(this.balance).toFixed(2), 
 				last_trade:'', 
 				last_profit:'', 
 				current_amount:this.amount
