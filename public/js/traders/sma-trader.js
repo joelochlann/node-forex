b258b17366876b75f6e2319431a2f9000ec1b039
hongfu <fu.lee.1987@gmail.com>
2015-06-26 10:50:10 +0100

    Fixed js to make it nicer.

--- public/js/traders/sma-trader.js
+++ public/js/traders/sma-trader.js
@@ -1,37 +1,23 @@
 window.SimpleMovingAverageTrader = (function(){
-    var self = this;
-
-    this.memorySize = 100000;
-
-    this.memory = [];
-
-    this.pushToMemory = function (tick) {
-        this.memory.unshift(tick);
-        // So we remove the last element from out memory if it exceed the memory size.
-        if (this.memory.length > this.memorySize) {
-            this.memory.pop();
-        }
-    };
-    var emptyMemory = function () {
-        self.memory = new Array();
-    };
-    var init = function() {
-        self.needInit=false;
-    };
-
     return Trader.extend({
 		buy:false,
 		sell:false,
         shortPeriod: 5,
         longPeriod: 10,
         defaultDecimalPoint: 1,
+        memory: [],
+        memorySize: 100000,
+        pushToMemory : function (tick) {
+            this.memory.unshift(tick);
+            // So we remove the last element from out memory if it exceed the memory size.
+            if (this.memory.length > this.memorySize) {
+                this.memory.pop();
+            }
+        },
 		buySignal: function(tick) {
             this.buy = false;
-            if(typeof self.need_init==='undefined') {
-                init();
-            }
-            self.pushToMemory(tick);
-            var sma = new Sma(this.shortPeriod,this.longPeriod, self.memory, this.defaultDecimalPoint);
+            this.pushToMemory(tick);
+            var sma = new Sma(this.shortPeriod,this.longPeriod, this.memory, this.defaultDecimalPoint);
             sma.parse();
             if (sma.goldenCrossHint) {
                 this.buy = true;
@@ -45,7 +31,7 @@ window.SimpleMovingAverageTrader = (function(){
 			if (this.buy) {
 				return false;
 			}
-            var sma = new Sma(this.shortPeriod,this.longPeriod, self.memory, this.defaultDecimalPoint);
+            var sma = new Sma(this.shortPeriod,this.longPeriod, this.memory, this.defaultDecimalPoint);
             sma.parse();
             if (sma.deathCrossHint) {
                 this.sell = true;
