2555bd90172b3632e3741f62c1b0b795324c73e3
Joseph Smith <joseph.smith@worldfirst.com>
2015-06-26 10:07:06 +0100

    Add complexity to sine wave, and improve appearance of graph

--- public/js/main.js
+++ public/js/main.js
@@ -84,8 +84,21 @@
         initCharts: function() {
             $('#price-chart').highcharts('StockChart', {
                 rangeSelector: {
-                    selected: 1
-                },
+		            buttons: [{
+		                count: 1,
+		                type: 'minute',
+		                text: '1M'
+		            }, {
+		                count: 5,
+		                type: 'minute',
+		                text: '5M'
+		            }, {
+		                type: 'all',
+		                text: 'All'
+		            }],
+		            inputEnabled: false,
+		            selected: 0
+		        },
                 title: {
                     text: 'GBP to USD Exchange Rate'
                 },
@@ -95,7 +108,7 @@
                         valueDecimals: 2
                     }
                 }]
-            });
+           });
         }
 
 
